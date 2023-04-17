87487ccdf95b3ba055ac4d808497e5dc99dacfc7
Amani Kilumanga <amani.kilumanga@gmail.com>
2023-04-17 22:33:41 +0200

    chore: added comment

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,6 +1,7 @@
 import { upgradePackageJson } from './util'
 
 describe('todo', () => {
+  // TODO restore test file after
   test('todo', async () => {
     await upgradePackageJson('test/sample.package.json')
   })
